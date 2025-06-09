9aa12c79446df8d7da4d2361db2b342fe6c0c203
Adolfo Gonzalez <aagp81@yahoo.es>
2025-06-09 15:56:21 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,17 +2,31 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-def buscar_estado_ot(numero_ot: str, tipo: str = "Orden de Transporte") -> list:
+
+def get_driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")  # Headless moderno
     options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(options=options)
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--remote-debugging-port=9222")
+    return webdriver.Chrome(options=options)
+
+def buscar_estado_ot(numero_ot: str, tipo: str = "Orden de Transporte") -> list:
+    
+    driver = get_driver()
 
     try:
         driver.get("http://sistema.tvp.cl/faces/logs_total.jsp")
-        time.sleep(2)
+        
+        # Espera hasta que esté presente el input
+        wait = WebDriverWait(driver, 10)
+        input_ot = wait.until(EC.presence_of_element_located((By.NAME, 'j_id_jsp_931588303_6:j_id_jsp_931588303_9')))
 
         # Ingresar número OT
         input_ot = driver.find_element(By.NAME, "j_id_jsp_931588303_6:j_id_jsp_931588303_9")
