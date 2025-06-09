14f24bc9f7a063000801349eb047d89855d1935a
Adolfo Gonzalez <aagp81@yahoo.es>
2025-06-09 16:55:02 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,19 +7,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def get_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
-    service = Service("/usr/local/bin/chromedriver")
-    return webdriver.Chrome(service=service, options=chrome_options)
 
 def buscar_estado_ot(numero_ot: str, tipo: str = "Orden de Transporte") -> list:
     
-    driver = get_driver()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/chromium"
+
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
         driver.get("http://sistema.tvp.cl/faces/logs_total.jsp")
